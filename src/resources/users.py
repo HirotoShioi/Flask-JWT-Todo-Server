58af64a761babb1bbf4de0715f0f1fe489dc74db
HirotoShioi <shioihigg@gmail.com>
2021-04-15 09:31:30 +0900

    Switch over to flask-jwt-extended

--- src/resources/users.py
+++ src/resources/users.py
@@ -1,6 +1,8 @@
 from flask_restful import Resource, reqparse
 from model.users import UserModel
-from flask_jwt import jwt_required, current_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 user_parser = reqparse.RequestParser()
 user_parser.add_argument("username", type=str, required=True, help="Username missing")
@@ -26,4 +28,26 @@ class UserRegister(Resource):
 class UserInformation(Resource):
     @jwt_required()
     def get(self) -> object:
-        return current_identity.json()
+        user_id = get_jwt_identity()
+        user = UserModel.find_by_id(user_id)
+        if user:
+            return user.json()
+        else:
+            return {"message": "User not found"}, 404
+
+
+class UserLogin(Resource):
+    def post(self) -> object:
+        data = user_parser.parse_args()
+
+        user = UserModel.find_by_username(data["username"])
+
+        # this is what the `authenticate()` function did in security.py
+        if user and safe_str_cmp(user.password, data["password"]):
+            # identity= is what the identity()
+            # function did in security.py—now stored in the JWT
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {"access_token": access_token, "refresh_token": refresh_token}, 200
+
+        return {"message": "Invalid Credentials!"}, 401
