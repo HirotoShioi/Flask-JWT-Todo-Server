394566e6c6b9848d5b3275a92a33a8c805c31891
HirotoShioi <shioihigg@gmail.com>
2021-04-15 12:05:03 +0900

    Add route for token refresh

--- src/app.py
+++ src/app.py
@@ -4,7 +4,12 @@ from flask_restful import Api
 from resources.todos import Todo, TodoManager
 from resources.users import UserLogin, UserRegister, UserInformation
 from db import db
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import (
+    JWTManager,
+    jwt_required,
+    get_jwt_identity,
+    create_access_token,
+)
 
 app = Flask(__name__)
 
@@ -16,13 +21,20 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 # Configuration END
 
 api = Api(app)
-
 jwt = JWTManager(app)
 
 
 @app.route("/")  # type: ignore
 def hello() -> object:
     return jsonify({"message": "Welcome to Todo APP"})
+
+
+@app.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True)
+def refresh() -> object:
+    identity = get_jwt_identity()
+    access_token = create_access_token(identity=identity, fresh=False)
+    return jsonify(access_token=access_token)
 
 
 api.add_resource(Todo, "/todo")
@@ -39,4 +51,4 @@ def create_tables() -> None:
 
 if __name__ == "__main__":
     db.init_app(app)
-    app.run(debug=True)
+    app.run(debug=False)
